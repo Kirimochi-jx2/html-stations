503077ce233058443272d9fa12083eb3e2594cb3
Kirimochi-jx2 <s2222035@stu.musashino-u.ac.jp>
2023-07-26 13:51:14 +0900

    add

--- src/scripts/station11-common.js
+++ src/scripts/station11-common.js
@@ -1,3 +1,11 @@
-var result = getData();
-var elem = document.getElementById("result");
-elem.innerHTML = result;
+var result = getData(); // ここでgetData関数を呼び出す。
+var elem = document.getElementById("result"); // ここでresultの値を表示する。
+elem.innerHTML = result; // ここでresultの値を表示する。
+/*getDataの中でhogehoge関数を呼び出す。*/
+function getData() {
+    return hogehoge();
+}
+/*hogehoge関数は変更せず、getData関数の中を変更する。*/
+function hogehoge() {
+    return "hogehoge";
+}
